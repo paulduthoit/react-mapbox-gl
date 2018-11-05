07e6770f2155c56d1fe7c9e92f7337bc578af70d
Paul Duthoit <duthoit.paul@gmail.com>
2018-11-05 11:25:57 +0100

    Fix bug

--- src/projected-layer.tsx
+++ src/projected-layer.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { createPortal } from 'react-dom';
-import * as PropTypes from 'prop-types';
 import { Map, Point } from 'mapbox-gl';
 import { OverlayParams, overlayState, overlayTransform } from './util/overlays';
 import { Anchor } from './util/types';
